e88b36402f9ccd9c62d88f46dec2e8a7772d9293
Agata Mostowik <agatamostowik@gmail.com>
2022-11-26 17:56:13 +0100

    changing port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { initApp } from "./src/app.js";
 
-const port = process.env.$PORT || 3001;
+const port = process.env.PORT || 3001;
 
 initApp().listen(port, () => {
   console.log(`aplikacja działa na http://localhost:${port}/`);
